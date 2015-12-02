6f034dd82ccea66bd3275d98a7bbe07651060fe7
wyqzl <wuyuqian19950217@qq.com>
2015-12-02 21:15:39 +0800

    point
    定义了一个指针变量，将100赋给a，输出a的地址

--- point/main.cpp
+++ point/main.cpp
@@ -10,6 +10,10 @@
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    int a,*p;
+    a=100;
+    p=&a;
+    printf("1 a=%x   $a=%x   &p=%x  p=%x  *p=%x\n",a,&a,&p,*p);
+    printf("2 a=%x   $a=%x   &p=%x  p=%x  *p=%x\n",a,&a,&p,*p);
     return 0;
 }
