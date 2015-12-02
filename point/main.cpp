8b3d110ae8cb199c09442a7037e4cef69310db78
wyqzl <wuyuqian19950217@qq.com>
2015-12-02 20:46:56 +0800

    Initial Commit

---
+++ point/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  point
+//
+//  Created by wyq on 15/12/2.
+//  Copyright (c) 2015年 wyq. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
